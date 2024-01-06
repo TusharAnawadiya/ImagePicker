4b60f303cd40231e4c674186d20afb4f9d7b4bc6
TusharAnawadiya <tusharanawadiya444@gmail.com>
2024-01-06 19:04:37 +0530

    Update Xcode Project

--- ImagePicker/ViewController.swift
+++ ImagePicker/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
+
+    @IBOutlet weak var photoInageView: UIImageView!
+    
+  //  let imagePicker = UIImagePickerController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+      //  imagePicker.delegate = self
     }
 
+    @IBAction func buttonAction(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+               imagePicker.sourceType = .photoLibrary
+               imagePicker.delegate = self
+               present(imagePicker, animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let selectedImage = info[.originalImage] as? UIImage {
+                photoInageView.image = selectedImage
+                if let imageURL = info[.imageURL] as? URL {
+                                print("Image URL: \(imageURL)")
+                            }
 
+            }
+            
+            picker.dismiss(animated: true, completion: nil)
+        }
 }
 
